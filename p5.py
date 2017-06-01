061e561a2d35e75e8805e3a7b1a72f7e8fbd544a
Mann <mann@angless.com>
2017-06-01 12:14:51 -0700

    created p5.py and main function in it for doing the main pipeline processing

---
+++ p5.py
@@ -0,0 +1,12 @@
+'''
+    train model, load saved model and do processing
+    on video to find vehicle in each frame
+'''
+def main():
+    print ("Hello world")
+
+
+# to prevent the file from executed when it import to other files     
+if __name__ == "__main__":
+    main()
+    
